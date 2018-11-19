ff60cc2acd4f8b142d580fd932ec9520776500a9
Ronald Post <ronald.post@magnaversum.com>
2018-11-19 08:51:56 +0100

    Added comments and key for the api

--- Funda/FundaService.cs
+++ Funda/FundaService.cs
@@ -7,18 +7,26 @@ using System.Net.Http.Headers;
 using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
 
 namespace Funda
 {
     public class FundaService : IFundaService
     {
+        private readonly IConfiguration _config;
+
+        public FundaService(IConfiguration config)
+        {
+            _config = config;
+        }
+        
         /// <summary>
-        /// 
+        /// Return a list of the real estate agents based on the filters, ordered by number of object.
         /// </summary>
         /// <param name="city"></param>
         /// <param name="filter"></param>
         /// <param name="count"></param>
-        /// <returns></returns>
+        /// <returns>List of RealEstate Agent instances</returns>
         public async Task<List<RealEstateAgent>> GetTopTenRealEstateAgents(string city, string filter, int count=10)
         {
             var realEstateAgents = new List<RealEstateAgent>();
@@ -38,6 +46,11 @@ namespace Funda
             return realEstateAgents.OrderByDescending(r => r.NumberOfObjects).Take(count).ToList();
         }
 
+        /// <summary>
+        /// Processes the data from the Funda API and adds or updates the number of objects.
+        /// </summary>
+        /// <param name="realEstateAgents"></param>
+        /// <param name="fundaRepoData"></param>
         private void ProcessRepoData(List<RealEstateAgent> realEstateAgents, FundaRepo fundaRepoData)
         {
             var grouped = fundaRepoData.Objects
@@ -64,6 +77,14 @@ namespace Funda
             }
         }
 
+        /// <summary>
+        /// Calls the Funda API to get the objects based on the filters.
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="city"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
         private async Task<FundaRepo> GetDataFromFunda(int page, int pageSize, string city, string filter)
         {
             using (var client = new HttpClient())
@@ -72,9 +93,8 @@ namespace Funda
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                const string key = "ac1b0b1572524640a0ecc54de453ea9f";
-
-                var requestUri = $"feeds/Aanbod.svc/json/{key}/?type=koop&zo=/{city}/{filter}/&page={page}&pagesize={pageSize}";
+                var fundaApiKey = _config["FundaApiKey"];                
+                var requestUri = $"feeds/Aanbod.svc/json/{fundaApiKey}/?type=koop&zo=/{city}/{filter}/&page={page}&pagesize={pageSize}";
 
                 var serializer = new DataContractJsonSerializer(typeof(FundaRepo));
                 var streamTask = client.GetStreamAsync(requestUri);
@@ -83,17 +103,26 @@ namespace Funda
         }
     }
 
+    /// <summary>
+    /// Data class to map the Funda api json result to an object.
+    /// </summary>
     public class FundaObject
     {
         public int MakelaarId { get; set; }
         public string MakelaarNaam { get; set; }
     }
 
+    /// <summary>
+    ///  Data class to map the Funda api paging result to an object.
+    /// </summary>
     public class Paging
     {
         public int AantalPaginas { get; set; }
     }
 
+    /// <summary>
+    /// Base data class to map the Funda api results.
+    /// </summary>
     public class FundaRepo
     {
         public List<FundaObject> Objects { get; set; }
--- TopRealEstateAgents/README.md
+++ TopRealEstateAgents/README.md
@@ -1,1 +1,16 @@
 ﻿
+# TopRealEstateAgents
+
+This is an application to show the top real estate agents for a given location. It's a dotnet core 2.1 application which uses Razor pages.
+
+
+## Set api key for development
+
+To set an api key for development purposes set the key as follows:
+
+
+  `dotnet user-secrets set FundaApiKey <key>`
+
+  for more information on storage of app secrets:
+ 
+  [Safe storage of app secrets in development in ASP.NET Core](https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-2.1&tabs=windows)
